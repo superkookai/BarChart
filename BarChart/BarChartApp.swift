f5631918a1ac1af3752c05e1b184d30f1e3d2e28
superkookai <weerawut@live.com>
2025-05-13 14:05:03 +0700

    Initial Commit

---
+++ BarChart/BarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartApp.swift
+//  BarChart
+//
+//  Created by Weerawut Chaiyasomboon on 13/05/2568.
+//
+
+import SwiftUI
+
+@main
+struct BarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
